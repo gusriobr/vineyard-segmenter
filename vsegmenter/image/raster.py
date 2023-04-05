6bed2029b79513798e9851c02286be3cfa16d52f
gusriobr <gusriobr@gmail.com>
2023-04-05 07:12:25 +0200

    first iteration completed

---
+++ vsegmenter/image/raster.py
@@ -0,0 +1,39 @@
+import numpy as np
+import rasterio
+from affine import Affine
+from rasterio.windows import Window
+
+
+def georeference_image(img, img_source, img_filename, scale=1, bands=3):
+    """
+    Creates a tiff raster to store the img param using geolocation
+    information from another raster.
+
+    :param img: Numpy array with shape [height, width, channels]
+    :param img_source: raster to take the geolocation information from.
+    :param img_filename: output raster filename
+    :param scale: scale rate to apply to output image
+    :return:
+    """
+
+    with rasterio.Env():
+        # read profile info from first file
+        dataset = rasterio.open(img_source)
+        meta = dataset.meta.copy()
+        dataset.close()
+
+        meta.update({"driver": "GTiff", "count": bands, 'dtype': 'uint8'})
+        meta.update({"width": img.shape[1], "height": img.shape[0]})
+        new_affine = meta["transform"] * Affine.scale(1 / scale, 1 / scale)
+        meta.update({"transform": new_affine})
+
+        print(meta)
+
+        with rasterio.open(img_filename, 'w', **meta, compress="JPEG") as dst: #, photometric="YCBCR"
+            for ch in range(img.shape[-1]):
+                # iterate over channels and write bands
+                img_channel = img[:, :, ch]
+                dst.write(img_channel, ch + 1)  # rasterio bands are 1-indexed
+
+
+
